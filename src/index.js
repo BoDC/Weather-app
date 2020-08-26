42b1c1eac1c042b8ce47bdb4ba3e499d7e67285d
Bo De Corte <bodecorte@icloud.com>
2020-08-26 20:46:18 +0200

    updated the date

--- src/index.js
+++ src/index.js
@@ -1,26 +1,34 @@
 /** @format */
 
-let now = new Date();
-let p = document.querySelector("p");
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
 
-let hours = now.getHours();
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${formatHours(timestamp)}`;
 }
 
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
-p.innerHTML = `${day} | ${hours}:${minutes}`;
+  return `${hours}:${minutes}`;
+}
 
 function showTemperature(response) {
   let temp = document.querySelector("#temperature");
@@ -37,6 +45,9 @@ function showTemperature(response) {
 
   let info = document.querySelector("#description");
   info.innerHTML = response.data.weather[0].description;
+
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 
   celsiusTemperature = response.data.main.temp;
 
